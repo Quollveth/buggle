e3c0135281baf9901a03db9b9998c9615104e334
Quollveth <quollveth@gmail.com>
2025-06-25 17:06:48 -0300

    hopefully paginator works

--- cmd/tui/config.go
+++ cmd/tui/config.go
@@ -8,6 +8,8 @@ type colors struct {
 	outBorder,
 	hightlight,
 	listActiveDot,
+	textPrimary,
+	textSecondary,
 	listInactiveDot lipgloss.Color
 }
 
@@ -17,18 +19,20 @@ type config struct {
 
 func createDefaultConfig() config {
 	return config{
-
 		colors: colors{
 			outBorder:       lipgloss.Color("#00FFFF"),
 			hightlight:      lipgloss.Color("#FFFF00"),
 			listActiveDot:   lipgloss.Color("252"),
 			listInactiveDot: lipgloss.Color("238"),
+			textPrimary:     lipgloss.Color("15"),
+			textSecondary:   lipgloss.Color("245"),
 		},
 	}
 }
 
 type styles struct {
 	middleWindow, bottomWindow,
+	textPrimary, textSecondary,
 	inactiveTab, activeTab lipgloss.Style
 }
 
@@ -46,13 +50,18 @@ func createStyles(colors colors, width, height int) styles {
 	inactiveTab := lipgloss.NewStyle().Border(inactiveTabBorder, true).BorderForeground(colors.outBorder).Padding(0, 1)
 	activeTab := inactiveTab.Border(activeTabBorder, true)
 
+	middleWindow := lipgloss.NewStyle().Width(width).Border(borderWithBottom("├", "─", "┤")).BorderForeground(colors.outBorder).UnsetBorderTop()
 	bottomWindow := lipgloss.NewStyle().Width(width).Border(lipgloss.RoundedBorder()).BorderForeground(colors.outBorder).UnsetBorderTop()
-	middleWindow := bottomWindow.UnsetBorderBottom()
+
+	textPrimary := lipgloss.NewStyle().Foreground(colors.textPrimary)
+	textSecondary := lipgloss.NewStyle().Foreground(colors.textSecondary)
 
 	return styles{
-		inactiveTab:  inactiveTab,
-		activeTab:    activeTab,
-		middleWindow: middleWindow,
-		bottomWindow: bottomWindow,
+		inactiveTab:   inactiveTab,
+		activeTab:     activeTab,
+		middleWindow:  middleWindow,
+		bottomWindow:  bottomWindow,
+		textPrimary:   textPrimary,
+		textSecondary: textSecondary,
 	}
 }
--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -13,9 +13,11 @@ import (
 )
 
 type song struct {
-	name   string
-	artist string
-	path   string
+	name     string
+	artist   string
+	album    string
+	path     string
+	duration int
 }
 
 type station struct {
@@ -47,11 +49,14 @@ type model struct {
 	styles styles
 
 	//-- UI data
+	firstRender         bool
 	winHeight, winWidth int
+	middleWinHeight     int
 
-	activeTab int
-	tabs      [NUM_TABS]string
-	tabView   [NUM_TABS]func(model) string
+	activeTab    int
+	tabs         [NUM_TABS]string
+	tabView      [NUM_TABS]func(model) string
+	selectedItem int
 
 	paginator paginator.Model
 }
@@ -62,65 +67,33 @@ func initModel(config config) model {
 	winWidth -= 2
 	winHeight -= 2
 
-	songs := []song{
-		{
-			name:   "Song 1",
+	songs := []song{}
+
+	for i := range 50 {
+		songs = append(songs, song{
+			name:   fmt.Sprint("Song ", i),
 			artist: "John Music",
-			path:   "music/song1.ogg",
-		},
-		{
-			name:   "Midnight Ride",
-			artist: "The Wanderers",
-			path:   "music/midnight_ride.ogg",
-		},
-		{
-			name:   "Echoes of Time",
-			artist: "Luna Nova",
-			path:   "music/echoes_of_time.ogg",
-		},
-		{
-			name:   "Coffeehouse Blues",
-			artist: "Ella and the Jazz Cats",
-			path:   "music/coffeehouse_blues.ogg",
-		},
-		{
-			name:   "Digital Dreams",
-			artist: "SynthRider",
-			path:   "music/digital_dreams.ogg",
-		},
+			album:  "Music - The Sequel",
+		})
 	}
 
-	stations := []station{
-		{
-			name: "Radio 1",
-			url:  "http://radio1.com/stream",
-		},
-		{
-			name: "Indie Vibes",
-			url:  "http://indievibes.fm/stream",
-		},
-		{
-			name: "Jazz Lounge",
-			url:  "http://jazzlounge.fm/stream",
-		},
-		{
-			name: "Synthwave Central",
-			url:  "http://synthcentral.net/stream",
-		},
-		{
-			name: "Classic Rock Live",
-			url:  "http://classicrocklive.fm/stream",
-		},
+	stations := []station{}
+
+	for i := range 75 {
+		stations = append(stations, station{
+			name: fmt.Sprint("Station ", i),
+		})
 	}
 
 	styles := createStyles(config.colors, winWidth, winHeight)
 
 	p := paginator.New()
 	p.Type = paginator.Dots
-	p.PerPage = 2
 	p.ActiveDot = lipgloss.NewStyle().Foreground(config.colors.listActiveDot).Render("•")
 	p.InactiveDot = lipgloss.NewStyle().Foreground(config.colors.listInactiveDot).Render("•")
-	p.SetTotalPages(len(songs))
+
+	p.PerPage = 3
+	p.SetTotalPages(0)
 
 	tabViews := [NUM_TABS]func(model) string{}
 
@@ -129,7 +102,8 @@ func initModel(config config) model {
 	tabViews[TAB_PLACEHOLDER] = placeholderView
 
 	return model{
-		connected: true,
+		firstRender: true,
+		connected:   true,
 		saved: struct {
 			songs    []song
 			stations []station
@@ -144,18 +118,33 @@ func initModel(config config) model {
 			song:    songs[0],
 			station: stations[0],
 		},
-		winWidth:  winWidth,
-		winHeight: winHeight,
+		winWidth:        winWidth,
+		winHeight:       winHeight,
+		middleWinHeight: 0,
 
 		styles:    styles,
 		paginator: p,
-		tabs:      [NUM_TABS]string{"tab 1", "tab 2", "tab 3"},
+		tabs:      [NUM_TABS]string{"Stations", "Songs", "Placeholder"},
 		tabView:   tabViews,
-		activeTab: 0,
+		activeTab: 1,
 	}
 }
 
 func (m model) Init() tea.Cmd { return nil }
+
+func (m *model) updatePaginator() {
+	m.paginator.Page = 0
+	m.selectedItem = 0
+
+	switch m.activeTab {
+	case TAB_STATIONS:
+		m.paginator.PerPage = 9
+		m.paginator.SetTotalPages(len(m.saved.stations))
+	case TAB_SONGS:
+		m.paginator.PerPage = 4
+		m.paginator.SetTotalPages(len(m.saved.songs))
+	}
+}
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
@@ -164,22 +153,40 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 
 		case "ctrl+c":
 			return m, tea.Quit
+
 		case "l":
+			m.selectedItem = 0
 			m.paginator.NextPage()
+
 		case "h":
+			m.selectedItem = 0
 			m.paginator.PrevPage()
+
+		case "k":
+			if m.selectedItem > 0 {
+				m.selectedItem -= 1
+			}
+
+		case "j":
+			if m.selectedItem < m.paginator.PerPage-1 {
+				m.selectedItem += 1
+			}
+
 		case "tab":
 			if m.activeTab == len(m.tabs)-1 {
 				m.activeTab = 0
 			} else {
 				m.activeTab++
 			}
+			m.updatePaginator()
+
 		case "shift+tab":
 			if m.activeTab == 0 {
 				m.activeTab = len(m.tabs) - 1
 			} else {
 				m.activeTab--
 			}
+			m.updatePaginator()
 		}
 	}
 
@@ -189,6 +196,25 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 //
 //-- Renders
 //
+
+func renderSong(song song, selected bool, styles styles) string {
+	var b strings.Builder
+
+	// lipgloss will stagger all the text if the newline is the last character but not if it's the first
+	var bullet string
+	if selected {
+		bullet = ">"
+	} else {
+		bullet = "•"
+	}
+
+	b.WriteString(styles.textPrimary.Render("   "+bullet, song.name))
+	b.WriteString(styles.textSecondary.Render("\n    ", song.artist))
+	b.WriteString(styles.textSecondary.Render("\n    ", song.album))
+	b.WriteString("\n\n")
+
+	return b.String()
+}
 
 func placeholderView(m model) string { return "nothing here" }
 
@@ -206,8 +232,8 @@ func stationsTabView(m model) string {
 func songTabView(m model) string {
 	var b strings.Builder
 	start, end := m.paginator.GetSliceBounds(len(m.saved.songs))
-	for _, item := range m.saved.songs[start:end] {
-		b.WriteString("  • " + item.name + "\n\n")
+	for idx, item := range m.saved.songs[start:end] {
+		b.WriteString(renderSong(item, m.selectedItem == idx, m.styles))
 	}
 	b.WriteString("  " + m.paginator.View())
 
@@ -245,7 +271,7 @@ func (m model) renderTabs() string {
 }
 
 func (m model) renderPlaying() string {
-	return "now playing goes here"
+	return fmt.Sprintf("tab: %v | per page: %v | total pages: %v", m.activeTab, m.paginator.PerPage, m.paginator.TotalPages)
 }
 
 //
@@ -263,20 +289,22 @@ func (m model) View() string {
 
 	tabsRow := lipgloss.JoinHorizontal(lipgloss.Top, allTabs, styledBorder)
 
+	playing := m.renderPlaying()
+
+	if m.firstRender {
+		m.middleWinHeight = m.winHeight - (lipgloss.Height(tabsRow) + lipgloss.Height(playing))
+		m.firstRender = false
+	}
+
 	//-- build
 
 	var b strings.Builder
-
-	playing := m.renderPlaying()
 
 	b.WriteString(lipgloss.JoinVertical(
 		lipgloss.Left,
 		tabsRow,
 
-		m.styles.middleWindow.Height(
-			m.winHeight-(lipgloss.Height(tabsRow)+lipgloss.Height(playing)),
-		).Render(m.tabView[m.activeTab](m)),
-
+		m.styles.middleWindow.Height(m.middleWinHeight).Render(m.tabView[m.activeTab](m)),
 		m.styles.bottomWindow.Render(playing),
 	))
 
@@ -284,7 +312,9 @@ func (m model) View() string {
 }
 
 func main() {
-	p := tea.NewProgram(initModel(createDefaultConfig()))
+	model := initModel(createDefaultConfig())
+
+	p := tea.NewProgram(model)
 
 	if _, err := p.Run(); err != nil {
 		fmt.Printf("peepee went poopoo: %v", err)
